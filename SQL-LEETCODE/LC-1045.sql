b7c56aad02d045edc01b910234b34e47155eae7c
safi-io <m.safi.ullah@outlook.com>
2025-06-25 14:15:06 +0500

    Grinding Leetcode Questions

---
+++ SQL-LEETCODE/LC-1045.sql
@@ -0,0 +1,19 @@
+
+
+SELECT CUSTOMER_ID FROM CUSTOMER
+GROUP BY CUSTOMER_ID
+HAVING COUNT(DISTINCT PRODUCT_KEY) >= (SELECT COUNT(*) FROM PRODUCT);
+
+SELECT PRODUCT_KEY FROM CUSTOMER;
+
+Create table If Not Exists Customer (customer_id int, product_key int);
+Create table Product (product_key int);
+Truncate table Customer;
+insert into Customer (customer_id, product_key) values ('1', '5');
+insert into Customer (customer_id, product_key) values ('2', '6');
+insert into Customer (customer_id, product_key) values ('3', '5');
+insert into Customer (customer_id, product_key) values ('3', '6');
+insert into Customer (customer_id, product_key) values ('1', '6');
+Truncate table Product;
+insert into Product (product_key) values ('5');
+insert into Product (product_key) values ('6');
---
+++ SQL-LEETCODE/LC-1070.sql
@@ -0,0 +1,15 @@
+
+
+;
+
+SELECT product_id, year AS first_year, quantity, price
+from sales
+WHERE (product_id, year) in (SELECT PRODUCT_ID, MIN(YEAR) AS FIRST_YEAR FROM SALES GROUP BY PRODUCT_ID HAVING MIN(YEAR
+));
+
+
+Create table If Not Exists Sales (sale_id int, product_id int, year int, quantity int, price int);
+Truncate table Sales;
+insert into Sales (sale_id, product_id, year, quantity, price) values ('1', '100', '2008', '10', '5000');
+insert into Sales (sale_id, product_id, year, quantity, price) values ('2', '100', '2009', '12', '5000');
+insert into Sales (sale_id, product_id, year, quantity, price) values ('7', '200', '2011', '15', '9000');
