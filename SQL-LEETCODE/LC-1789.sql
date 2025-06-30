b0c05dad0d4d18c4488e3c2a022f82d82c36defc
safi-io <m.safi.ullah@outlook.com>
2025-06-30 20:52:32 +0500

    I Love LC Questions

---
+++ SQL-LEETCODE/LC-1789.sql
@@ -0,0 +1,24 @@
+
+
+
+
+
+SELECT EMPLOYEE_ID, DEPARTMENT_ID FROM employee
+WHERE PRIMARY_FLAG = 'Y' OR EMPLOYEE_ID IN (
+SELECT EMPLOYEE_ID FROM EMPLOYEE
+GROUP BY EMPLOYEE_ID
+HAVING COUNT(*) < 2
+);
+
+
+Create table If Not Exists Employee (employee_id int, department_id int, primary_flag ENUM('Y','N'));
+Truncate table Employee;
+insert into Employee (employee_id, department_id, primary_flag) values ('1', '1', 'N');
+insert into Employee (employee_id, department_id, primary_flag) values ('2', '1', 'Y');
+insert into Employee (employee_id, department_id, primary_flag) values ('2', '2', 'N');
+insert into Employee (employee_id, department_id, primary_flag) values ('3', '3', 'N');
+insert into Employee (employee_id, department_id, primary_flag) values ('4', '2', 'N');
+insert into Employee (employee_id, department_id, primary_flag) values ('4', '3', 'Y');
+insert into Employee (employee_id, department_id, primary_flag) values ('4', '4', 'N');
+
+
